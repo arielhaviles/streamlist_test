884711ce228f277ce344169273c45659537e2881
arielhaviles <ariel_0694@hotmail.com>
2026-07-04 15:03:00 -0600

    version 3.1

--- app.py
+++ app.py
@@ -14,14 +14,16 @@ def toss_coin(n):
     outcome_no = 0
     outcome_1_count = 0
 
-    for r in trial_outcomes:
-        outcome_no +=1
-        if r == 1:
-            outcome_1_count += 1
-        mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
-        time.sleep(0.05)
+placeholder = st.empty()
 
+for r in trial_outcomes:
+    outcome_no += 1
+    if r == 1:
+        outcome_1_count += 1
+    mean = outcome_1_count / outcome_no
+    
+    placeholder.line_chart([mean])
+    time.sleep(0.05)
     return mean
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
@@ -30,3 +32,4 @@ start_button = st.button('Ejecutar')
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
     mean = toss_coin(number_of_trials)
+
